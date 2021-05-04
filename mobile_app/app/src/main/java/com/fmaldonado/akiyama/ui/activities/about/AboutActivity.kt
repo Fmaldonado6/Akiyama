5080f27f3e8055c7309bc35c10ca69ba66c200cc
Fernando Maldonado <fmaldonado824@gmail.com>
2021-05-04 00:17:23 -0500

    Added icon credits

--- mobile_app/app/src/main/java/com/fmaldonado/akiyama/ui/activities/about/AboutActivity.kt
+++ mobile_app/app/src/main/java/com/fmaldonado/akiyama/ui/activities/about/AboutActivity.kt
@@ -3,6 +3,7 @@ package com.fmaldonado.akiyama.ui.activities.about
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import com.fmaldonado.akiyama.R
 import com.fmaldonado.akiyama.databinding.ActivityAboutBinding
 import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
@@ -18,5 +19,6 @@ class AboutActivity : AppCompatActivity() {
             startActivity(Intent(this, OssLicensesMenuActivity::class.java))
         }
         binding.backButton.setOnClickListener { finish() }
+        binding.iconLink.movementMethod = LinkMovementMethod.getInstance()
     }
 }
