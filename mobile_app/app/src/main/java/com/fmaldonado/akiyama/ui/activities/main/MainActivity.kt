5e8c283ce3dd2afe37bc06a2ed35d3be339dffe9
Fernando Maldonado <f_maldonado6@hotmail.com>
2022-06-11 21:57:15 -0500

    Removed compose xd

---
+++ mobile_app/app/src/main/java/com/fmaldonado/akiyama/ui/activities/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.fmaldonado.akiyama.ui.activities.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fmaldonado.akiyama.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
