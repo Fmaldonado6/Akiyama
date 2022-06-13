4640e1292d78772aaa4989208e07af32ed4bef26
Fernando Maldonado <f_maldonado6@hotmail.com>
2022-06-13 14:09:57 -0500

    Added search bar

---
+++ mobile_app/app/src/main/java/com/fmaldonado/akiyama/ui/activities/seacrhAnime/SearchAnimeActivity.kt
@@ -0,0 +1,12 @@
+package com.fmaldonado.akiyama.ui.activities.seacrhAnime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fmaldonado.akiyama.R
+
+class SearchAnimeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_anime)
+    }
+}
