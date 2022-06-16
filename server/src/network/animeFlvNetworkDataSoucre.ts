9ff238b637942d73a01c89cf44311996523e0940
Fernando Maldonado <f_maldonado6@hotmail.com>
2022-06-16 13:17:22 -0500

    Added args

--- server/src/network/animeFlvNetworkDataSoucre.ts
+++ server/src/network/animeFlvNetworkDataSoucre.ts
@@ -9,7 +9,14 @@ export class AnimeFlvNetworkDataSource {
 
     protected async init(): Promise<puppeteer.Page> {
         if (!this.browser)
-            this.browser = await puppeteer.launch({ args: ['--no-sandbox'], headless: true })
+            this.browser = await puppeteer.launch({
+                args: [
+                    '--no-sandbox',
+                    '--disable-setuid-sandbox',
+                    '--disable-dev-shm-usage',
+                    '--single-process'
+                ], headless: true
+            })
         const page = await this.browser.newPage();
         page.setDefaultNavigationTimeout(30000);
         await page.setUserAgent('Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0')
